ad57d30b070fc90e9872419f576931f90d9ebd2f
ethicalhub <yasiransari.elith@gmail.com>
2026-03-29 15:44:23 +0530

    feat: add navigation layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import {styled} from "nativewind";
 
+const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-primary">
         Welcome to Nativewind!
       </Text>
@@ -26,6 +29,6 @@ export default function App() {
       >
         Sign Up
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
